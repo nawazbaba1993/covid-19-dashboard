77ab792709eefd4d0afb09a607c5bee86aeb3a5a
Nawaz Baba <nawaz@ginee.dev>
2021-03-26 07:45:17 +0530

    initial setup

---
+++ src/test/java/com/wonder/coviddashboard/CovidDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonder.coviddashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
